63b86484b269bdd848cf37874bb53aff01e3947b
AnnaKalis <annkalis@gmail.com>
2023-09-19 13:30:27 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// question 1
+var number1 = 10;
+var number2 = 5.5;
+
+
+// question 2
+var workingDays = "Monday" + "-" + "Tuesday"+ "-" + "Wednesday"+ "-" +"Thursday"+ "-" +   "Friday";
+console.log("workingDays:", workingDays);
+
+// question 3
+var addition = number1 + number2;
+console.log("addition:" , addition);
+
+var subtraction = number1 - number2;
+console.log("subtraction:", subtraction);
+
+var multiplication = number1 * number2;
+console.log("multiplication:", multiplication);
+
+var division = number1 / number2; 
+console.log("division:", division);
+
+// question 4
+var onSale = false;
+console.log(typeof onSale);
+
+// question 5
+var string1 ="The frogs are angry";
+console.log ("String lenght:" ,string1.length);
+
+// question 6
+var timeElapsed
+var totalPrice
+
+// question 7
+var number3 ="53";
+var number4 ="44.6";
+
+var convertedNumber3 = parseInt(number3);
+var convertedNumber4 = parseFloat(number4);
+
+console.log(convertedNumber3);
+console.log(convertedNumber4);
+
+console.log (typeof number3);
+console.log(typeof number4);
+console.log(typeof convertedNumber3);
+console.log(typeof convertedNumber4);
+
+// question 8
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+console.log("Pet:", typeof pet);
+console.log("Age:", typeof age);
+console.log("Discount:", typeof discountApplied);
+console.log("Time Left:", typeof timeLeft);
+
+// question 9
+var heading = "Updating heading";
+
+var updatingHeading = document.querySelector("h1");
+
+updatingHeading.innerHTML = heading;
+
+console.log(updatingHeading);
+
+// question 10
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue";
